dcc67a30a0486415657fbd9bdde8c4826d57153b
Admin <kolavlasuk848@gmail.com>
2026-03-31 21:07:49 +0300

    PassManager 1.1
    + defence
    + login

--- main.cpp
+++ main.cpp
@@ -2,6 +2,12 @@
 #include <iostream>
 #include <windows.h>
 
+std::string get_self_path() {
+    char buffer[MAX_PATH];
+    GetModuleFileName(NULL, buffer, MAX_PATH);
+    return std::string(buffer);
+}
+
 std::string getHWID() {
     DWORD hwid = 0;
     GetVolumeInformationA("C:\\",NULL,0,&hwid,NULL,NULL,NULL,0);
@@ -47,7 +53,7 @@ std::string enc(std::string data,char key) {
 }
 
 void add_to_db(std::string name,std::string login,std::string email,std::string pass) {
-    std::ofstream file("PassManager.exe:db",std::ios::app);
+    std::ofstream file(get_self_path() + ":db",std::ios::app);
     if (file.is_open()) {
         char key = 35;
         std::string ENCpass = enc(pass,key);
@@ -64,7 +70,7 @@ void add_to_db(std::string name,std::string login,std::string email,std::string 
 }
 
 void find_pass(std::string target_name) {
-    std::ifstream file("PassManager.exe:db");
+    std::ifstream file(get_self_path() + ":db");
     std::string name, login,email,encpass;
     char key= 35;
     bool found = false;
@@ -95,7 +101,7 @@ void find_pass(std::string target_name) {
 }
 
 void clear_db() {
-    std::ofstream file("PassManager.exe:db", std::ios::trunc);
+    std::ofstream file(get_self_path() + ":db", std::ios::trunc);
     if (file.is_open()) {
         file.close();
         print("База данных полностью очищена!");
@@ -104,9 +110,70 @@ void clear_db() {
     }
 }
 
+// void login() {
+//     std::string master_path = get_self_path() + ":master";
+//     std::string saved_master,input_pass;
+//     char key = 77;
+//
+//     std::ifstream check_file(master_path);
+//
+//     if (!check_file.is_open()) {
+//         print("\n\n==== ПЕРВЫЙ ЗАПУСК ====");
+//         input_pass = input("Введите МАСТЕР-КЛЮЧ: ");
+//         std::ofstream out(master_path);
+//         if (out.is_open()) {
+//             out << enc(input_pass,key);
+//             out.close();
+//             print("[УСПЕХ] Программа будет перезапущена!");
+//             Sleep(600);
+//             exit(0);
+//         } else {
+//             std::getline(check_file,saved_master);
+//             check_file.close();
+//
+//             while (true) {
+//                 system("cls");
+//                 print("\n==== ВХОД В СИСТЕМУ ====");
+//                 input_pass = input("Введите ваш МАСТЕР-КЛЮЧ: ");
+//
+//                 if (enc(input_pass,key) == saved_master) {
+//                     print("[ДОСТУП РАЗРЕШЕН]");
+//                 } else {
+//                     print("[ДОСТУП НЕ РАЗРЕШЕН]");
+//                     Sleep(700);
+//                     exit(0);
+//                 }
+//             }
+//         }
+//     }
+//
+// }
+
+void full_reset() {
+    std::string confirm = input("Вы уверены, что хотите удалить все данные? (y/n): ");
+    if (confirm == "y" | confirm == "Y") {
+        std::string paths[] = {
+            get_self_path() + ":db",
+            get_self_path() + ":id",
+            get_self_path() + ":master",
+        };
+        for (const std::string& p : paths) {
+            std::ofstream f(p,std::ios::trunc);
+            f.close();
+        }
+
+        print("Все данные стерты, Программа закрывается!");
+        Sleep(1000);
+        exit(0);
+    }
+}
+
 int main() {
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
+
+    // login();
+
     std::string current_hwid = getHWID();
     std::string pass;
     std::string login;
@@ -116,11 +183,12 @@ int main() {
     BOOL isDebuggerPresent;
     char key = 35;
 
+
     CheckRemoteDebuggerPresent(GetCurrentProcess(), &isDebuggerPresent);
 
-    std::ifstream file("PassManager.exe:id");
+    std::ifstream file(get_self_path() + ":id");
     if (!file.is_open()) {
-        std::ofstream out("PassManager.exe:id");
+        std::ofstream out(get_self_path() + ":id");
         out << current_hwid;
         out.close();
 
@@ -151,6 +219,10 @@ int main() {
         showmenu();
 
         choice = input("Выберите действие: ");
+        if (choice.empty()) {
+            continue;
+        }
+
 
         if (choice == "1") {
             name = input("Название соцсети: ");
