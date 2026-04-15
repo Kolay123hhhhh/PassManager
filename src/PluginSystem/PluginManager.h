8468981fb58650d7a4122f3f3d6fa7f15ae277a4
Admin <kolavlasuk848@gmail.com>
2026-04-15 15:20:58 +0300

    PassManager 2.9 обновил исходники

---
+++ src/PluginSystem/PluginManager.h
@@ -0,0 +1,89 @@
+//
+// Created by Admin on 05.04.2026.
+//
+
+#ifndef PASSMANAGER_PLUGINMANAGER_H
+#define PASSMANAGER_PLUGINMANAGER_H
+
+
+#include <iostream>
+#include <string>
+#include <filesystem>
+#include <windows.h>
+#include <cstdlib>
+#include <functional>
+
+class PluginManager {
+public:
+
+    struct PluginAPI {
+        void (*print)(std::string text) = [](std::string text) {
+            std::cout << text << std::endl;
+        };
+
+        void (*setColor)(int color) = [](int color) {
+            HANDLE hmod = GetStdHandle(STD_OUTPUT_HANDLE);
+
+            SetConsoleTextAttribute(hmod, color);
+        };
+
+        std::string (*input)(std::string promt) = [](std::string promt) {
+            std::string value;
+            std::cout << promt;
+            std::cin >> value;
+            return value;
+        };
+        // std::string (*getDataPath)(std::string promt) = [](std::string promt) {
+        //
+        // };
+        void (*addPassword)(std::string name,std::string login,std::string email,std::string pass) = [](std::string name,std::string login,std::string email,std::string pass) {};
+        void (*clearDB)() = []() {};
+        void (*findPass)(std::string target_mame) = [](std::string target_mame) {};
+        void (*exportDB)(std::string name_file) = [](std::string name_file) {};
+        void (*createFile)(std::string name_of_file) = [](std::string name_of_file) {};
+
+
+
+        void (*cls)() = []() {system("cls");};
+        void (*pause)() = []() {system("pause");};
+        void (*onExit)() = []() { exit(0);};
+        // void (*selfUnload)() = []() {};
+        std::function<void()> selfUnload;
+
+    };
+
+    virtual void onInit() {};
+    virtual void onMenu() {};
+    virtual void onUpdate() {};
+    virtual void onAuthSuccess() {};
+    virtual void onAuthFailure() {};
+    virtual void onHwidSuccess() {};
+    virtual void onHwidFailure() {};
+    virtual void onPreExit() {};
+    virtual void onClearDB() {};
+
+
+    virtual void onPasswordAdded(std::string name,std::string login,std::string email,std::string pass) {};
+
+    virtual std::string getMenuItemName() {
+        return "Неизвестный плагин";
+    };
+
+
+    virtual void onMenuSelected(PluginAPI api) = 0;
+
+    int pluginID = 0;
+    void setPluginID(int id) { pluginID = id; }
+
+
+    virtual bool handleChoice(const std::string& choice) {
+        return false;
+    }
+
+    virtual ~PluginManager() {}
+};
+
+
+typedef PluginManager* (__cdecl *CreatePluginFunc)();
+
+#endif //PASSMANAGER_PLUGINMANAGER_H
---
+++ src/UpdateSystem/UpdaterClass.cpp
@@ -0,0 +1,75 @@
+//
+// Created by Admin on 10.04.2026.
+//
+
+#include "UpdaterClass.h"
+#include <iostream>
+#include <fstream>
+#include <windows.h>
+#include "../utils.h"
+
+#pragma comment(lib, "urlmon.lib")
+
+void UpdaterClass::checkUpdates() {
+    std::string VersionGit_url = "https://raw.githubusercontent.com/bogdashs/PassManager/refs/heads/master/version.txt";
+    std::string DownloadGit_url = "https://github.com/bogdashs/PassManager/releases/latest/download/PassManager.exe";
+
+    std::string tempVersion = "Data\\check.tmp";
+    std::string tempDownload = "Data\\PassManager_new.exe";
+    setColor(11);
+    print("[ИНФО] Проверка обновлений");
+    setColor(7);
+
+    if (URLDownloadToFileA(NULL,VersionGit_url.c_str(),tempVersion.c_str(),0,NULL) == S_OK) {
+
+        std::ifstream file(tempVersion);
+        std::string remoteVersion;
+        file >> remoteVersion;
+        remoteVersion.erase(remoteVersion.find_last_not_of('\n\r\t') + 1);
+        file.close();
+        DeleteFileA(tempVersion.c_str());
+
+        if (!remoteVersion.empty() && remoteVersion != CURRENT_VERSION) {
+            setColor(14);
+            print("[ОБНОВЛЕНИЕ] Доступна новая версия: " + remoteVersion);
+            setColor(7);
+            std::string choice = input("Желаете обновиться? (y/n): ");
+
+            if (choice == "y" || choice == "Y") {
+                setColor(11);
+                print("[ИНФО] Доступна новая версия\n");
+                setColor(7);
+
+                if (URLDownloadToFileA(NULL, DownloadGit_url.c_str(),tempDownload.c_str(),0,NULL) == S_OK) {
+                    setColor(10);
+                    print("[УСПЕХ] Версия скачена!");
+                    Sleep(2000);
+                    setColor(7);
+
+                    std::string args = "PassManager.exe " + tempDownload;
+
+                    ShellExecuteA(NULL,"open","Updater.exe",args.c_str(),NULL,SW_SHOW);
+
+                    exit(0);
+
+                } else {
+                    setColor(12);
+                    print("[УСПЕХ]");
+                    system("pause");
+                    exit(0);
+                }
+
+            }
+
+
+        } else {
+            setColor(11);
+            print("[ИНФО] У вас установлена актуальная версия");
+            setColor(7);
+        }
+
+
+
+    }
+
+}
